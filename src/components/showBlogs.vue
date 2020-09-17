dc2bf003f4871aad1085b4aedbb1f76314581d04
mazhar-rony <mazhar.rony@gmail.com>
2020-09-17 10:17:01 +0600

    Add Filters

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,9 +1,14 @@
 <template>
-  <div v-theme:column="'narrow'" id="show-blogs">
+  <!-- custom directive
+  <div v-theme:column="'narrow'" id="show-blogs"> -->
+  <div id="show-blogs">
     <h1>All Blog Articles</h1>
     <div v-for="blog in blogs" v-bind:key="blog" class="single-blog">
-        <h2 v-rainbow>{{ blog.title }}</h2>
-        <article>{{ blog.body }}</article>
+        <!-- custom directive
+        <h2 v-rainbow>{{ blog.title }}</h2> -->
+        <!-- filters -->
+        <h2>{{ blog.title | to-uppercase }}</h2>
+        <article>{{ blog.body | snippet }}</article>
     </div>
   </div>
 </template>
